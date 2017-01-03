71659495094955a428ffafe45f7272241fabdb76
Duc N. PHAM <pngduc@gmail.com>
2017-01-03 10:58:05 +0700

    Config routes

--- app/index.js
+++ app/index.js
@@ -1,8 +1,27 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import HelloComponent from './components/hello.jsx';
+import {
+  Router,
+  Route,
+  IndexRoute,
+  browserHistory,
+  hashHistory
+} from 'react-router';
 
-ReactDOM.render(
-  <HelloComponent />,
+import AppComponent from './components/App.jsx';
+import HomeComponent from './components/Home.jsx';
+import ContactComponent from './components/Contact.jsx';
+import AboutComponent from './components/About.jsx';
+
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={AppComponent}>
+      <IndexRoute component={HomeComponent} />
+
+      <Route path="/contact/:target" component={ContactComponent} />
+      <Route path="/about" component={AboutComponent} />
+    </Route>
+  </Router>
+  ),
   document.getElementById('app')
 );
